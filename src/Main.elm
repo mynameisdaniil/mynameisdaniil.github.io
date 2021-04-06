7d5256e5c687af27cfee6cce8bbee69ce85d85d2
mynameisdaniil <sobol.daniil@gmail.com>
2021-04-07 01:01:47 +0300

    new version

--- src/Main.elm
+++ src/Main.elm
@@ -14,8 +14,7 @@ import Time
 import Tuple exposing (pair, first, second)
 import Yaml.Decode exposing (map3, fromString, list, dict, string, field)
 
-overhead = 5
-email = concat ["sobol", ".", "daniil", "@", "gmail", ".", "com"] -- fuck spammers
+overhead = 7
 
 type alias Link        = String
 type alias Icon        = String
@@ -23,7 +22,7 @@ type alias Description = String
 
 type Msg = YamlLoaded (Result Http.Error String) | RandomList (List Int) | None
 
-type alias Model = {facts : List String, social : List (Link, Icon), links: List (Description, Link), randomness: List Int}
+type alias Model = {facts : List String, social : List (Link, Icon), links: List (Description, Link), email: List String, randomness: List Int}
 
 tuple_extractor a b = \v -> (withDefault "" <| get a v, withDefault "" <| get b v)
 
@@ -52,7 +51,7 @@ subscriptions model = Sub.none
 init : () -> (Model, Cmd Msg)
 init _ =
   let
-    initialState = {facts = [], social = [], links = [], randomness = []}
+    initialState = {facts = [], social = [], links = [], email = ["sobol", ".", "daniil", "@", "gmail", ".", "com"], randomness = []}
     commands = Cmd.batch [
       Http.get {url = "../contents.yaml", expect = Http.expectString YamlLoaded}
       ]
@@ -82,16 +81,57 @@ view model = {title = "Hello world", body = body model}
 
 body model =
   let
-      email_block = [div [] [text "The best way to contact me: ", a [href <| concat ["mailto:", email]] [text email]]]
-      blocks = [display_facts model.facts, display_social model.social, display_links model.links, email_block]
+      blocks = [display_facts model.facts, display_social model.social, display_links model.links, display_email model.email, display_source_code]
       (shuffled_blocks, residual_randomness) = foldl (\block (acc, randomness) ->
         (shuffle block randomness :: acc, drop (length block) randomness)
         ) ([], model.randomness) blocks
   in
-    map (div [style "border-radius" "5pt", style "border" "1pt solid #000000", style "padding" "10pt", style "margin" "10pt", style "display" "block", style "position" "absolute"]) <| shuffle shuffled_blocks residual_randomness
+    map (div body_style) <| shuffle shuffled_blocks residual_randomness
 
-display_facts = map (\str -> p [] [ text str ])
+make_style = map (\(a, b) -> style a b)
 
-display_social = map (\(link, icon) -> p [] [img [src icon, style "height" "20pt", style "vertical-align" "middle"] [], a [href link, style "height" "14pt", style "vertical-align" "middle"] [text link] ])
+body_style = make_style [
+  ("border-radius", "5pt"),
+  ("border", "1pt solid #000000"),
+  ("padding", "10pt"),
+  ("margin", "10pt"),
+  ("display", "inline-block")
+  ]
 
-display_links = map (\(description, link) -> p [] [ text description, text ": ", a [href link] [text link] ])
+display_facts = map (\str -> p facts_style [ text str ])
+
+facts_style = make_style [
+  ("height", "1.5em")
+  ]
+
+display_social = map (\(link, icon) -> p [] [img (src icon :: social_img_style) [], a (href link :: social_link_style) [text link] ])
+
+social_img_style = make_style [
+  ("height", "2em"),
+  ("vertical-align", "middle")
+  ]
+
+social_link_style = make_style [
+  ("height", "1.5em"),
+  ("vertical-align", "middle")
+  ]
+
+display_links = map (\(description, link) -> p links_style [ text description, text ": ", a [href link] [text link] ])
+
+links_style = make_style [
+  ("height", "1.5em")
+  ]
+
+display_email parts = [div email_style [text "The best way to contact me: ", a [href <| concat <| "mailto:" :: parts] [text <| concat parts]]]
+
+email_style = make_style [
+  ("height", "1.5em")
+  ]
+
+source_code_link = "https://github.com/mynameisdaniil/mynameisdaniil.github.io/blob/master/src/Main.elm"
+
+display_source_code = [div source_code_style [text "This page is written in Elm: ", a [href source_code_link] [text source_code_link]]]
+
+source_code_style = make_style [
+  ("height", "1.5em")
+  ]
